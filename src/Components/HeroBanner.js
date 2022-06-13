9b029c5c577714f822b1cc41389995f0adfd3338
Maazasim1 <58209878+Maazasim1@users.noreply.github.com>
2022-06-14 04:07:18 +0500

    second commit

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -2,47 +2,75 @@ import Civic from './civic.jpg';
 import Corolla from './corolla.jpg';
 import Interior from './interior.jpg';
 import './HeroBanner.scss'
-import {useState} from 'react'
+import { useState } from 'react'
+import { motion, AnimatePresence } from "framer-motion";
 
 function HeroBanner() {
-    const [imageNumber,setImageHover]=useState(Civic);
+    const [imageNumber, setImageHover] = useState(Civic);
 
     const changeImage = (e) => {
-    {
-        const name=e.target.className;
-        switch (name) {
-            case 'clean':
-                setImageHover(Civic);
+        {
+            const name = e.target.className;
+            switch (name) {
+                case 'clean':
+                    setImageHover(Civic);
 
 
-            case 'inter':
-                setImageHover(Interior);
+                case 'inter':
+                    setImageHover(Interior);
 
-            case 'service':
-                setImageHover(Corolla)
+                case 'service':
+                    setImageHover(Corolla)
 
-                
-                break;
-        
-            default:
-                break;
+
+                    break;
+
+                default:
+                    break;
+            }
         }
-    }
     }
 
 
     return (
         <div className='mainContainer'>
-                <img className="tinted-image1" src={imageNumber} alt="CarCleaners" />
+            <AnimatePresence>
+                <motion.img
+                    key={imageNumber}
+                    src={imageNumber}
+                    className="tinted-image1"
+                    initial={{y:1000, scaleX:1,
+                        scaleY:1,}}
+                    animate={{
+                        zIndex: 0,
+                        y: 0,
+                        opacity: 1,
+                       
+                        
+                    }}
+                    exit={{
+                        zIndex: 0,
+                        y:  -1000,
+                        opacity: 0,
+                        scaleX:0.7,
+                        scaleY:0.7,
+                    }}
+                    transition={{
+                        y: { type: "spring", stiffness: 150, damping: 40 },
+                        opacity: { duration: 0.5 }
+                    }}
+                />
+            </AnimatePresence>
 
-        
+
+
             <div className='selector'>
                 <h4>What We Do</h4>
-                <a  className='clean'>Cleaning</a>
-                <br/>
-                <a onMouseEnter={()=>setImageHover(Interior)} onMouseLeave={()=>setImageHover(Civic)}  className='inter'>Interior Cleaning</a>
-                <br/>
-                <a onMouseEnter={()=>setImageHover(Corolla)}  onMouseLeave={()=>setImageHover(Civic)}  className='service'>Service</a>
+                <a className='clean'>Cleaning</a>
+                <br />
+                <a onMouseEnter={() => setImageHover(Interior)} onMouseLeave={() => setImageHover(Civic)} className='inter'>Interior Cleaning</a>
+                <br />
+                <a onMouseEnter={() => setImageHover(Corolla)} onMouseLeave={() => setImageHover(Civic)} className='service'>Service</a>
 
             </div>
 
--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -1,9 +1,16 @@
 import './navBar.scss';
-import Logo from './carcleanersLogo.jpeg'
+import Logo from './carcleanersLogo.png'
+import { motion } from "framer-motion"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCalendarCheck);
 
 
 
 function NavBar() {
+  
     return (
         <div className="NavBar">
             <nav>
@@ -13,6 +20,15 @@ function NavBar() {
                     <li><a href="#!">About</a></li>
                     <li><a href="#!">Contact</a></li>
                     <li><a href="#!">Faq</a></li>
+                    <div>
+                    <motion.a whileHover={{ color: 'red' }} whileTap={{ scale: 0.9 }} className="book">
+                    <FontAwesomeIcon icon="calendar-check" className='iconCal'/>
+                        BOOK APPOINTMENT
+           
+                    </motion.a>
+
+                    </div>
+
                 </ul>
             </nav>
 
